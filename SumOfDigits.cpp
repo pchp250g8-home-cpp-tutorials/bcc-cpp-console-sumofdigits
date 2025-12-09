cc64a985fa1c9b382c5bfec9abee624a038ec75f
pchp250g8 <pchp250g8@tutamail.com>
2025-12-09 16:47:02 +0300

    SumOfDigits C++ Builder Project

---
+++ SumOfDigits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <tchar.h>
+#include <math.h>
+#include <stdlib.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int iNum, iVal, iSumDigits;
+    system("cls");
+    std::cout << "Input an integer number\r\n";
+    std::cin >> iNum;
+    iVal = abs(iNum); iSumDigits = 0;
+    while (iVal > 0)
+    {
+        iSumDigits += iVal % 10;
+        iVal /= 10;
+    }
+    std::cout << "The sum of digits of the number " << iNum << " is "
+              << iSumDigits << "\r\n";
+    std::cin.ignore();
+    std::cin.get();
+}
